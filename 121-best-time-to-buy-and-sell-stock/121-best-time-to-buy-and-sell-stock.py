f276ac1c6cbd97fafb75112f00a5605b88a9f8ce
Nikita Siahaan <58053548+nsiahaan@users.noreply.github.com>
2022-08-01 23:03:01 -0700

    Time: 1273 ms (76.91%), Space: 25 MB (38.44%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profits = set()
+        profit = 0
         target = prices[-1] # start at end of list
         for i in range(len(prices) - 1, -1, -1):
             # for each item in the list, compare with target and record profit. update target to greatest #
             if prices[i] > target: 
                 target = prices[i]
             else:
-                profits.add(target - prices[i])
-        return max(profits)
+                if (target - prices[i]) > profit:
+                    profit = target - prices[i]
+        return profit
