9f3338ead6f95f37971d5c1956e82b262e06d679
Nikita Siahaan <58053548+nsiahaan@users.noreply.github.com>
2022-10-11 17:28:25 -0700

    Time: 70 ms (68.72%), Space: 14 MB (42.76%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,6 +1,21 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        # binary search approach, use L + R ptrs, for each L/R check if > neighbors -> return L/R else move on
+        # binary search -> need midpoint approach
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[mid + 1]: 
+                if nums[mid] > nums[mid - 1]: # peak
+                    return mid
+                else: # reduce search space in half
+                    right = mid
+            else:
+                left = mid + 1
+        return left
+        
+        '''
+        # realized this is O(n)
+        # use L + R ptrs, for each L/R check if > neighbors -> return L/R else move on
         if len(nums) == 1:
             return 0
         
@@ -20,3 +35,4 @@ class Solution:
             else: # no peak
                 left += 1
                 right -= 1
+        '''
